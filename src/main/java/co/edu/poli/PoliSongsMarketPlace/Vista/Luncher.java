c28922f9163c5f00841ec40997c600253a96efec
dplazas11 <dplazas@poligran.edu.co>
2025-11-22 07:35:39 -0500

    correccion de archivos

---
+++ src/main/java/co/edu/poli/PoliSongsMarketPlace/Vista/Luncher.java
@@ -0,0 +1,13 @@
+
+package co.edu.poli.PoliSongsMarketPlace.Vista;
+
+
+public class Luncher {
+    
+    public static void main (String [] args){
+        
+        App.main(args);
+        
+    }
+    
+}
